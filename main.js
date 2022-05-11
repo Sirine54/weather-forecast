32deb52d7db3ffbae3b5e18962da6185649df780
Sirine Saoudi <95916760+Sirine54@users.noreply.github.com>
2022-05-11 18:00:31 +0200

    add geolocation

--- main.js
+++ main.js
@@ -3,14 +3,17 @@ let weather = {
     fetchWeather: function(city){
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+
        city+'&appid=3d5ad915bd588ead9ffe6f7eee2cbc9c')
+     
         .then((response)=> response.json())
+        
         .then((data)=>
             this.displayWeather(data)
        )
        
         ;
     },
-
+   
+ 
     displayWeather:function(data){
         const {name} = data;
         const {icon,description} = data.weather[0]
@@ -28,6 +31,7 @@ let weather = {
         document.body.style.background= "linear-gradient(rgba(0, 0, 1, 0.426),rgba(0, 0, 1, 0.415)),url('https://source.unsplash.com/1600x900/?" + name + "')";
         
     } , 
+    
     search:function(){
         let searchCity = document.querySelector('#search-Bar').value
         if(searchCity===''){alert('Invalid Input')}
@@ -38,6 +42,71 @@ let weather = {
     }
 
 };
+/////////////////Copy code from open Cage ////////////////////////
+
+
+let geocode ={
+
+    reverseGeocode:function(latitude,longitude){
+
+    var api_key = '4ee15a8fd25844fd889cf6f3f1849073';
+    
+    var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+
+  var request_url = api_url
+    + '?'
+    + 'key=' + api_key
+    + '&q=' + encodeURIComponent(latitude + ',' + longitude)
+    + '&pretty=1'
+    + '&no_annotations=1';
+
+  // see full list of required and optional parameters:
+  // https://opencagedata.com/api#forward
+
+  var request = new XMLHttpRequest();
+  request.open('GET', request_url, true);
+
+  request.onload = function() {
+    // see full list of possible response codes:
+    // https://opencagedata.com/api#codes
+
+    if (request.status === 200){
+      // Success!
+      var data = JSON.parse(request.responseText);
+      //console.log(data.results[0].components.country)
+        weather.fetchWeather(data.results[0].components.country)
+
+    } else if (request.status <= 500){
+      // We reached our target server, but it returned an error
+
+      console.log("unable to geocode! Response code: " + request.status);
+      var data = JSON.parse(request.responseText);
+      console.log('error msg: ' + data.status.message);
+    } else {
+      console.log("server error");
+    }
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+    console.log("unable to connect to server");
+  };
+
+  request.send();  // make the request
+},
+getLocation:function(){
+    function success(data){
+        geocode.reverseGeocode(data.coords.latitude,data.coords.longitude);
+    }
+    if(navigator.geolocation){
+       navigator.geolocation.getCurrentPosition(success, console.error);
+    }else{
+        weather.fetchWeather("tunisia")
+    }
+}
+
+}
+
 
 ////////////////////Add event listener to search button //////////////////////
 
@@ -53,5 +122,16 @@ document.addEventListener("keyup",function(e){
         weather.search()
     }
 })
+///////////////////Add DAte //////////////////////////////
 
-weather.fetchWeather("Tunisia")
+const dateElement = document.getElementById("date");
+const options={weekday:"long", month:"short", day:"numeric"}
+const toDay = new Date();
+dateElement.innerHTML = toDay.toLocaleDateString("en-US",options)
+
+
+
+
+
+
+geocode.getLocation()
